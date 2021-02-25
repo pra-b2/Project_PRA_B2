ceb29b38dadbbcf1582703442a15137a8f26f2a7
basvandenmaagdenberg <71122658+BasvandenMaagdenberg@users.noreply.github.com>
2021-02-25 11:12:49 +0100

    achtergrondkleur veranderd
    achtergrond veranderd naar lichtblauw

--- WachtrijApp/FormWachtrij.Designer.cs
+++ WachtrijApp/FormWachtrij.Designer.cs
@@ -103,7 +103,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Peru;
+            this.BackColor = System.Drawing.Color.LightBlue;
             this.ClientSize = new System.Drawing.Size(279, 227);
             this.Controls.Add(this.labelKar1);
             this.Controls.Add(this.labelKar2);
